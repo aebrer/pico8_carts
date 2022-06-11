267121a7b3a7238ab359806b344398a90d85f38e
aebrer <andrew@brereton.me>
2022-06-11 16:37:59 -0400

    crush is done af its soooo good

--- fxhash/the crush/index.js
+++ fxhash/the crush/index.js
@@ -26,21 +26,47 @@ let hgt;
 let hc;
 let ww;
 let wh;
-let x=-16;
-let y=-16;
+let x;
+let y;
 let col;
-let lc=0;
-let c;
-let num_col=16;
 let shred_count;
 let shred_lim;
 let splay_n;
 let water_n;
 let pd=2;
+let need_preview;
+let bgc;
+let initial_run=true;
+window.$fxhashFeatures = {}
+
+//fxhash features
+let recursive_shred;
+let hori_tear;
+let vert_tear;
+let c;
 let calt;
+let prim_col_dir;
+let sec_col_dir;
+let fullscreen;
+let xfac;
+let noaa;
 
 function setup() {
   
+  x=-16;
+  y=-16;
+  need_preview=true;
+  window.$fxhashFeatures = {}
+
+  recursive_shred=false;
+  hori_tear=false;
+  vert_tear=false;
+  prim_col_dir=false;
+  sec_col_dir=false;
+  fullscreen=false;
+  xfac=8.5
+  noaa=true
+
   fxrand = sfc32(...hashes)
   
   shred_count=0;
@@ -48,11 +74,63 @@ function setup() {
   splay_n=random_int(10,15);
   water_n=random_int(10,15);
 
+  while (!(recursive_shred || hori_tear || vert_tear)){
+    recursive_shred = random_int(0,100)>35
+    hori_tear = random_int(0,100)>35
+    vert_tear = random_int(0,100)>60
+  }
+
+  window.$fxhashFeatures["Recursive Shredding"] = recursive_shred
+  window.$fxhashFeatures["Horizontal Tear"] = hori_tear
+  window.$fxhashFeatures["Vertical Tear"] = vert_tear
 
   // tweak the palette for a bit of variation
   c = [random_int(0,360),100,100,1]
-  calt = randomChoice([0,90,120,180])
+  calt = randomChoice([0,10,15,25,45,90,120,180,225,270])
+  window.$fxhashFeatures["Primary Hue"] = c[0]
+  window.$fxhashFeatures["Hue Offset"] = calt
   console.log(calt)
+
+  // color direction
+  if (random_num(0,1)>.5){
+    prim_col_dir=true
+    window.$fxhashFeatures["Primary Color Direction"] = "Increasing"
+    c[1]=25
+    c[2]=50
+  } else {
+    window.$fxhashFeatures["Primary Color Direction"] = "Decreasing"
+  }
+
+    // color direction
+  if (random_num(0,1)>.5){
+    sec_col_dir=true
+    window.$fxhashFeatures["Secondary Color Direction"] = "Same"
+  } else {
+    window.$fxhashFeatures["Secondary Color Direction"] = "Different"
+  }
+
+  window.$fxhashFeatures["Awareness of the Edge"] = "Maintained"
+  if (random_num(0,1)>.9) {
+    fullscreen=true
+    window.$fxhashFeatures["Awareness of the Edge"] = "Violated"
+  }
+  
+  if (initial_run) {
+      let rnum=random_int(1,100)
+      if (rnum>97){
+        bgc=[random_int(0,360),random_int(0,100),random_int(0,100),random_num(0,1)]
+        window.$fxhashFeatures["Background"]="Random"
+      } else if (rnum>94) {
+        bgc=[0,100,100,1]
+        window.$fxhashFeatures["Background"]="White"
+      } else if (rnum>91) {
+        bgc=[0,0,0,1]
+        window.$fxhashFeatures["Background"]="Black"
+      } else {
+        bgc=[0,0,0,0]
+        window.$fxhashFeatures["Background"]="Transparent"
+      }
+  }
 
   if(isFxpreview){
     ww=1920
@@ -72,16 +150,19 @@ function setup() {
 
   pg.pixelDensity(1);
   pixelDensity(pd);
-  // blendMode(DIFFERENCE);
+  blendMode(BLEND);
   noSmooth();
-  pg.background(0);
+  pg.background(bgc);
   pg.strokeWeight(1)  
+
+  console.table(window.$fxhashFeatures)
 
 }
 
 
 
 function draw() {
+  background(bgc[0],bgc[1],bgc[2],0)
   if(hc>hgt+10){
 
     blendMode(DIFFERENCE)
@@ -92,41 +173,59 @@ function draw() {
       if (random_int(1,1000)>997)fxrand=sfc32(...hashes)
       if (random_int(1,1000)>997)fxrand=sfc32(...hashes)
 
-      //splay effect
-      for (let i=0;i<splay_n;i++) {
-        x=random_int(0,ww)
-        y=random_int(0,wh)
-        image(this, x+random_num(-ww/32,ww/32),y+random_num(-wh/32,wh/32),ww/32,wh/32, x+random_num(-ww/32,ww/32),y+random_num(-wh/32,wh/32),random_num(ww/32,ww/32),random_num(wh/32,wh/32))
+
+      if (recursive_shred){
+        //splay effect
+        for (let i=0;i<splay_n;i++) {
+          x=random_int(0,ww)
+          y=random_int(0,wh)
+          image(this, x+random_num(-ww/32,ww/32),y+random_num(-wh/32,wh/32),ww/32,wh/32, x+random_num(-ww/32,ww/32),y+random_num(-wh/32,wh/32),random_num(ww/32,ww/32),random_num(wh/32,wh/32))
+        }
       }
       // splay_n*=0.99
 
       // // // water vfx
-      // for (let i=0;i<water_n;i++) {
-      //   let y=random_int(0,wh)
-      //   image(this, 0, y, ww, wh/1024, random_int(-5,5), y, ww, wh/1024)
-      // }
-
-      // // // water vfx
-      // for (let i=0;i<water_n;i++) {
-      //   let x=random_int(0,ww)
-      //   image(this, x, 0, ww/1024, wh, x, random_int(-5,5), ww/1024, wh)
-      // }
+      if (hori_tear){
+        for (let i=0;i<water_n;i++) {
+          let y=random_int(0,wh)
+          image(this, 0, y, ww, wh/1024, random_int(-5,5), y, ww, wh/1024)
+        }
+      }
+      if (vert_tear) {
+        // // // water vfx
+        for (let i=0;i<water_n;i++) {
+          let x=random_int(0,ww)
+          image(this, x, 0, ww/1024, wh, x, random_int(-5,5), ww/1024, wh)
+        }
+      }
 
       shred_count+=1
 
       return
     } else {
-      fxpreview()
+      if(need_preview){
+        fxpreview()
+        need_preview=false
+      }
       return
    }
   }
 
   x=0
-  for (i=0;i<=wth/8.5;i++) {
+  if (fullscreen) {x=-wth;xfac=1}
+  for (i=0;i<=wth/xfac;i++) {
    pg.noStroke()
-   let col = randomChoice(
-      [c, [(c[0]+calt)%360, c[1], c[2], c[3]]]
-    )
+
+   let col;
+   if (sec_col_dir){
+     col = randomChoice(
+        [c, [(c[0]+calt)%360, c[1], c[2], c[3]]]
+      )
+    } else {
+      col = randomChoice(
+        [c, [(c[0]+calt)%360, 100-c[1], 100-c[2], c[3]]]
+      )
+    }
    pg.fill(col)
    x+=random_num(-10,10)
    y=hc+random_num(-0.1,0.1)
@@ -135,14 +234,61 @@ function draw() {
    pg.ellipse(x, y, random_num(-5,5), random_num(-5,5))
    pg.rect(x, y, random_num(-5,5), random_num(-5,5))
    
-
   }
   image(pg, 0, 0, ww, wh, 0, 0, wth, hgt)
-  c = [c[0],c[1]*random_num(0.999,0.9999),c[2]*random_num(0.993,0.999),c[3]]
+  if (prim_col_dir) {
+    c = [c[0],c[1]*random_num(1.0001,1.001),c[2]*random_num(1.0005,1.001),c[3]]
+  } else {
+    c = [c[0],c[1]*random_num(0.999,0.9999),c[2]*random_num(0.993,0.999),c[3]]
+  }
+  
 
   hc += random_num(1,2)
 }
 
-
-
-// function keyPressed(){var e;"e"==key?save("Yo_automata_"+fxhash+".png"):"h"==key&&(pixelDensity(8),snew=800,e=snew/size,resizeCanvas(snew*iratio,snew),pg.scale(e),save("Yo_automata_highRes_"+fxhash+".png"))}
+function keyTyped() {
+  if (key === 's') {
+    save(mycan, "ELSRRFFGTS_DTB.png")
+  } else if (key === "1") {
+    pd=1
+    setup()
+  } else if (key === "2") {
+    pd=2
+    setup()
+  } else if (key === "3") {
+    pd=3
+    setup()
+  } else if (key === "4") {
+    pd=4
+    setup()
+  } else if (key === "5") {
+    pd=5
+    setup()
+  } else if (key === "6") {
+    pd=6
+    setup()
+  } else if (key === "7") {
+    pd=7
+    setup()
+  } else if (key === "8") {
+    pd=8
+    setup()
+  } else if (key === "t") {
+    initial_run=false
+    bgc=[0,0,0,0]
+    setup()
+  } else if (key === "w") {
+    initial_run=false
+    bgc=[0,100,100,1]
+    setup()
+  } else if (key === "b") {
+    bgc=[0,0,0,1]
+    initial_run=false
+    setup()
+  } else if (key === "r") {
+    bgc=[random_int(0,360),random_int(0,100),random_int(0,100),random_num(0,1)]
+    console.log(bgc)
+    initial_run=false
+    setup()
+  }
+}
