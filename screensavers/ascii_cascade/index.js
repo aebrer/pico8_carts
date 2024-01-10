dd0f0b0fd0cf503c8b6623c769c6b1ba3f4d400c
aebrer <andrew@brereton.me>
2024-01-10 08:49:44 -0500

    copilot assisted refactor

--- screensavers/ascii_cascade/index.js
+++ screensavers/ascii_cascade/index.js
@@ -1,4 +1,3 @@
-// Entropy Locked Wave Function Collapse
 
 /*
 Controls:
@@ -18,278 +17,147 @@ license: CC0 -> go nuts; citations not required but definitely appreciated
 */
 
 
-//thanks @Yazid for these two helper functions
-function random_num(a, b) {
-    return a+(b-a)*fxrand()
+
+// Helper functions
+const random_num = (a, b) => a + (b - a) * fxrand();
+const random_int = (a, b) => Math.floor(random_num(a, b + 1));
+const randomChoice = arr => arr[Math.floor(random_num(0, 1) * arr.length)];
+
+const get_new_hashes = () => {
+  fxhash = "oo" + Array(49).fill(0).map(_ => alphabet[(Math.random() * alphabet.length) | 0]).join('');
+  const b58dec = str => [...str].reduce((p, c) => p * alphabet.length + alphabet.indexOf(c) | 0, 0);
+  const fxhashTrunc = fxhash.slice(2);
+  const regex = new RegExp(".{" + ((fxhashTrunc.length / 4) | 0) + "}", 'g');
+  const hashes = fxhashTrunc.match(regex).map(h => b58dec(h));
+  return hashes;
+};
+
+const is_mobile = window.matchMedia("(any-hover: none)").matches;
+
+fxrand = sfc32(...hashes);
+window.$fxhashFeatures = {};
+
+let pg, wth, hgt, hc, ww, wh, x, y, col, pd = 5, dd, initial_run = true, mycan, tx, c, calt, nostroke, loop_count = 0, locking_method, xdata, ydata, pixeldata, seed_freq;
+// const ent_lock_methods = ["Random Chance", "Consistent by Frame Count"];
+const ent_lock_methods = ["None"];
+
+
+const get_hsb = (h, s, b) => `hsb(${h},${s}%,${b}%)`;
+
+const get_random_pixel_by_state = state => randomChoice(get_all_pixels_by_state(state));
+
+const get_all_pixels_by_state = state => {
+  let good_pixels = [];
+  for (let x = 0; x < wth; x++) {
+    let pixels = pixeldata[x];
+    good_pixels = [...good_pixels, ...pixels.filter(pixel => pixel.state === state)];
   }
-function random_int(a, b) {
-  return Math.floor(random_num(a, b+1))
-}
+  return good_pixels;
+};
+// This function will return the possible neighbor colors given a color
+const get_possible_colors = col => {
+  const h = hue(col);
+  const s = saturation(col);
+  const b = brightness(col);
 
+  const possible_hue_transforms = [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, randomChoice([0, 0, 180])];
+  const possible_hues = possible_hue_transforms.map(x => Math.floor((h + x + 360) % 360));
 
-function randomChoice(arr) {
-  return arr[Math.floor(random_num(0,1) * arr.length)];
-}
+  let possible_saturation_transforms = [1, 2, 3];
+  possible_saturation_transforms = possible_saturation_transforms.concat(possible_saturation_transforms.map(x => -x));
+  const possible_saturations = possible_saturation_transforms.map(x => Math.floor(Math.max(0, Math.min(100, s + x))));
 
-function get_new_hashes() {
-  fxhash = "oo" + Array(49).fill(0).map(_=>alphabet[(Math.random()*alphabet.length)|0]).join('')
-  b58dec = str=>[...str].reduce((p,c)=>p*alphabet.length+alphabet.indexOf(c)|0, 0)
-  fxhashTrunc = fxhash.slice(2)
-  regex = new RegExp(".{" + ((fxhashTrunc.length/4)|0) + "}", 'g')
-  hashes = fxhashTrunc.match(regex).map(h => b58dec(h))
-  return hashes
-}
+  let possible_brightness_transforms = [1, 2, 3, 5, 10];
+  possible_brightness_transforms = possible_brightness_transforms.concat(possible_brightness_transforms.map(x => -x));
+  const possible_brightnesses = possible_brightness_transforms.map(x => Math.floor(Math.max(0, Math.min(100, b + x))));
 
+  const possible_colors = possible_hues.map(hue => possible_saturations.map(saturation => possible_brightnesses.map(brightness => color(get_hsb(hue, saturation, brightness))))).flat(2);
 
-// will decide on mobile mode unless there is a pointer device with hover capability attached
-let is_mobile = window.matchMedia("(any-hover: none)").matches
+  return possible_colors;
+};
 
-// hashes = "hriirieiririiiritiififiviviifj"
-// if(hashes==="debug"){hashes=random_num(0,1000000)}
-fxrand = sfc32(...hashes)
-window.$fxhashFeatures = {}
+// This function will set the color of a given pixel
+const set_pixel_color = (x, y, col) => {
+  pixeldata[x][y].color = col;
+  pixeldata[x][y].state = "settled";
+  pg.set(x, y, col);
 
-//PGrahics object
-let pg;
+  const neighbors = [
+    [(x - 1 + wth) % wth, y],
+    [(x + 1 + wth) % wth, y],
+    [x, (y - 1 + hgt) % hgt],
+    [x, (y + 1 + hgt) % hgt],
+    [(x + 1 + wth) % wth, (y + 1 + hgt) % hgt],
+    [(x - 1 + wth) % wth, (y - 1 + hgt) % hgt],
+    [(x + 1 + wth) % wth, (y - 1 + hgt) % hgt],
+    [(x - 1 + wth) % wth, (y + 1 + hgt) % hgt]
+  ];
 
-let wth;
-let hgt;
-let hc;
-let ww;
-let wh;
-let x;
-let y;
-let col;
-let pd=5;
-let dd;
-
-let initial_run=true;
-
-let mycan;
-let tx;
-
-//fxhash features
-let c;
-let calt;
-let nostroke;
-let loop_count=0;
-let locking_method;
-
-let xdata;
-let ydata;
-let pixeldata;
-let seed_freq;
-// let ent_lock_methods = ["Random Chance", "Consistent by Frame Count", "None"]
-let ent_lock_methods = ["Random Chance", "Consistent by Frame Count"]
-// let ent_lock_methods = ["Consistent by Frame Count"]
-
-// function get hsb color string
-function get_hsb(h, s, b) {
-  return "hsb(" + h + "," + s + "%," + b + "%)";
-}
-
-
-// this function will return a random pixel from the given state
-function get_random_pixel_by_state(state) {
-  let good_pixels = get_all_pixels_by_state(state)
-  return randomChoice(good_pixels)
-}
-
-
-// this function will return x,y coords of ALL pixels of a given state
-function get_all_pixels_by_state(state) {
-  let good_pixels = []
-  // for each x position get the list of pixels
-  for (let x=0; x<wth; x++) {
-    let pixels = pixeldata[x]
-    // for each pixel in the list of pixels, if the pixel state matches the given state, add it to the good_pixels array
-    pixels.forEach(pixel => {
-      if (pixel.state === state) {
-        good_pixels.push(pixel)
-      }
-    })
-  }
-  return good_pixels
-}
-
-// this function will return the possible neighbor colors given a color
-function get_possible_colors(col) {
-  // get the hue, saturation, and brightness of the color
-  let h = hue(col)
-  let s = saturation(col)
-  let b = brightness(col)
-
-  let possible_colors = []
-  // hue transformations modulo 360
-  // let possible_hue_transforms = [1,2,3,5,7,10,180]
-  let possible_hue_transforms = [0,0,0,0,0,0,0,0,0,1,2,randomChoice([0,0,180])]
-  // let possible_hue_transforms = [0]
-  // add a negative version for each hue transformation
-  // possible_hue_transforms = possible_hue_transforms.concat(possible_hue_transforms.map(x => -x))
-  // for each hue transformation, add the transformed hue to the possible_hues
-  let possible_hues = possible_hue_transforms.map(x => Math.floor((h+x+360)%360))
-
-
-  // hue transformations modulo 100
-  let possible_saturation_transforms = [1,2,3]
-  // let possible_saturation_transforms = [0]
-  // add a negative version for each hue transformation
-  possible_saturation_transforms = possible_saturation_transforms.concat(possible_saturation_transforms.map(x => -x))
-  // for each saturation transformation, add the transformed saturation to the possible_saturations, min 0, max 100
-  let possible_saturations = possible_saturation_transforms.map(x => Math.floor(Math.max(0, Math.min(100, s+x))))
-
-  // brightness transformations modulo 100
-  let possible_brightness_transforms = [1,2,3,5,10]
-  // let possible_brightness_transforms = [0]
-  // add a negative version for each brightness transformation
-  possible_brightness_transforms = possible_brightness_transforms.concat(possible_brightness_transforms.map(x => -x))
-  // for each brightness transformation, add the transformed brightness to the possible_brightnesss, min 0, max 100
-  let possible_brightnesses = possible_brightness_transforms.map(x => Math.floor(Math.max(0, Math.min(100, b+x))))
-
-  // for each possible hue, saturation, and brightness, create a color and add it to the possible_colors array
-  possible_hues.forEach(hue => {
-    possible_saturations.forEach(saturation => {
-      possible_brightnesses.forEach(brightness => {
-        possible_colors.push(color(get_hsb(hue, saturation, brightness)))
-      })
-    })
-  })
-
-  // return the possible colors
-  return possible_colors
-}
-
-
-// this function will set the color of a given pixel
-function set_pixel_color(x, y, col) {
-
-  // set the pixel's color
-  pixeldata[x][y].color = col
-  // set the pixel's state to settled
-  pixeldata[x][y].state = "settled"
-
-  // update the pixels on the graphics object
-  pg.set(x,y,col)
-
-  // calculate the x and y coordinates of the pixel's neighbors using vectorized math
-  let neighbors = [
-    [(x-1+wth)%wth, y], 
-    [(x+1+wth)%wth, y], 
-    [x, (y-1+hgt)%hgt], 
-    [x, (y+1+hgt)%hgt], 
-    [(x+1+wth)%wth, (y+1+hgt)%hgt], 
-    [(x-1+wth)%wth, (y-1+hgt)%hgt], 
-    [(x+1+wth)%wth, (y-1+hgt)%hgt], 
-    [(x-1+wth)%wth, (y+1+hgt)%hgt]
-  ]
-  // for each neighbor, check if it is in bounds
-  for (let i = 0; i < neighbors.length; i++) {
-    let neighbor = neighbors[i]
-    if (pixeldata[neighbor[0]][neighbor[1]].state != "settled") {
-      // if the neighbor is not settled, add to it's possible colors based on this pixels color
-      pixeldata[neighbor[0]][neighbor[1]].colors.push(...get_possible_colors(col))
-      // // drop duplicates
-      // pixeldata[neighbor[0]][neighbor[1]].colors = [...new Set(pixeldata[neighbor[0]][neighbor[1]].colors)]
-      // set the neighbor's state to waiting
-      pixeldata[neighbor[0]][neighbor[1]].state = "waiting"
+  neighbors.forEach(([nx, ny]) => {
+    if (pixeldata[nx][ny].state != "settled") {
+      pixeldata[nx][ny].colors.push(...get_possible_colors(col));
+      pixeldata[nx][ny].state = "waiting";
     }
-  }
-
-
-}
-
+  });
+};
 
 function setup() {
-  
+  fxrand = sfc32(...hashes);
 
-  fxrand = sfc32(...hashes)
-  
-
-  if(isFxpreview){
-    ww=1080
-    wh=1080
-    is_mobile=false
-    pd=5
+  if (isFxpreview) {
+    ww = 1080;
+    wh = 1080;
+    is_mobile = false;
+    pd = 5;
   } else {
-    ww=windowWidth
-    wh=windowHeight
+    ww = windowWidth;
+    wh = windowHeight;
   }
 
   mycan = createCanvas(ww, wh);
 
-  wth = random_int(16,32)
-  // wth=16
-  window.$fxhashFeatures["Pixel Width"] = wth
-  // wth = 32
-  hgt = Math.ceil(wth * (wh/ww))
-  hc=-wth
+  wth = 32;
+  window.$fxhashFeatures["Pixel Width"] = wth;
+  hgt = Math.ceil(wth * (wh / ww));
+  hc = -wth;
   pg = createGraphics(wth, hgt);
-  pg.colorMode(HSL)
+  pg.colorMode(HSL);
 
-  dd=displayDensity()
-  let df = Math.ceil(dd * pd * 0.5)
-  if(is_mobile){df/=3}
+  dd = displayDensity();
+  let df = Math.ceil(dd * pd * 0.5);
+  if (is_mobile) { df /= 3; }
   pixelDensity(df);
-  // blendMode(DIFFERENCE);
   noSmooth();
   pg.background(0);
-  pg.strokeWeight(1)  
+  pg.strokeWeight(1);
 
-  // get a seed frequency, higher is fewer seeds
+  let seed_freq = random_int(5, 100);
 
-  let seed_freq = random_int(5, 100)
-  // window.$fxhashFeatures["seed_freq"] = seed_freq
+  locking_method = randomChoice(ent_lock_methods);
 
-  locking_method = randomChoice(ent_lock_methods)
-  // locking_method = "None"
-  // window.$fxhashFeatures["Entropy Locking Method"] = locking_method
-
-  // seed pixel acceptable hues
-  hues = [random_int(0,360)]
-  // add some selections of various transformations
-  for(let i = 0; i < 3; i++){
-    hues.push(hues[0]+randomChoice([5,10,15,0,0,0,0,0,0,0,0,0,45,45,45,120,120,180,180,180,180,270,270])%360)
+  hues = [random_int(0, 360)];
+  for (let i = 0; i < 3; i++) {
+    hues.push((hues[0] + randomChoice([5, 10, 15, 0, 0, 0, 0, 0, 0, 0, 0, 45, 45, 45, 120, 120, 180, 180, 180, 180, 270, 270])) % 360);
   }
 
-  // get the number of unique hues
-  let num_hues = [...new Set(hues)].length
-  window.$fxhashFeatures["Number of Seed Hues"] = num_hues
+  let num_hues = [...new Set(hues)].length;
+  window.$fxhashFeatures["Number of Seed Hues"] = num_hues;
 
+  pixeldata = Array.from({ length: wth }, (_, i) => Array.from({ length: hgt }, (_, j) => ({ x: i, y: j, state: "unseen", colors: [], color: color(get_hsb(calt, 0, 0)) })));
 
-  // pixeldata
-  // each pixel is an object with, x,y, state (unseen, waiting, settled), and possible colors (an array which will be updated later)
-  // there are wth by hgt pixels
-  // we will specifically select pixels based on their state at various times, so it needs to be effecient
-  // we will also need to be able to select a random pixel from a given state
+  console.table(window.$fxhashFeatures);
+};
 
-  // initialize pixeldata array with correct dimensions and all pixels in unseen state
-  pixeldata = new Array(wth)
-  for (let i = 0; i < wth; i++) {
-    pixeldata[i] = new Array(hgt)
-    for (let j = 0; j < hgt; j++) {
-      pixeldata[i][j] = {x: i, y: j, state: "unseen", colors: [], color: color(get_hsb(calt,0,0))}
-    }
-  }
+const renew_pixels = () => {
+  pixeldata.forEach(row => row.forEach(pixel => {
+    pixel.state = "unseen";
+    pixel.colors = [randomChoice(pixel.colors)];
+  }));
 
-  console.table(window.$fxhashFeatures)
-}
-
-function renew_pixels(){
-  // set all pixels to unseen
-  for (let i = 0; i < wth; i++) {
-    for (let j = 0; j < hgt; j++) {
-      pixeldata[i][j].state = "unseen"
-      pixeldata[i][j].colors = [randomChoice(pixeldata[i][j].colors)]
-    }
-  }
-  // get a new hash
-  hashes = get_new_hashes()
-  // set the random seed
-  fxrand = sfc32(...hashes)
-  // get a new entropy locking method
-  locking_method = randomChoice(ent_lock_methods)
-
-}
+  hashes = get_new_hashes();
+  fxrand = sfc32(...hashes);
+  locking_method = randomChoice(ent_lock_methods);
+};
 
 function draw() {
   
@@ -343,7 +211,7 @@ function draw() {
   // render the graphics buffer to the display canvas
   image(pg, 0, 0, ww, wh, 0, 0, wth, hgt)
 
-  blendMode(SUBTRACT);
+  blendMode(DIFFERENCE);
   for (let i=0;i<64;i++) {
     x=random_int(0,ww)
     y=random_int(0,wh)
@@ -359,33 +227,20 @@ function draw() {
   return
 }
 
-// ux
 function keyTyped() {
-  if (key === 's') {
-    save(mycan, "export_.png")
-  } else if (key === "1") {
-    pd=1
-    setup()
-  } else if (key === "2") {
-    pd=2
-    setup()
-  } else if (key === "3") {
-    pd=3
-    setup()
-  } else if (key === "4") {
-    pd=4
-    setup()
-  } else if (key === "5") {
-    pd=5
-    setup()
-  } else if (key === "6") {
-    pd=6
-    setup()
-  } else if (key === "7") {
-    pd=7
-    setup()
-  } else if (key === "8") {
-    pd=8
-    setup()
-  } 
+  const actions = {
+    's': () => save(mycan, "export_.png"),
+    '1': () => { pd = 1; setup(); },
+    '2': () => { pd = 2; setup(); },
+    '3': () => { pd = 3; setup(); },
+    '4': () => { pd = 4; setup(); },
+    '5': () => { pd = 5; setup(); },
+    '6': () => { pd = 6; setup(); },
+    '7': () => { pd = 7; setup(); },
+    '8': () => { pd = 8; setup(); },
+  };
+
+  if (actions[key]) {
+    actions[key]();
+  }
 }
