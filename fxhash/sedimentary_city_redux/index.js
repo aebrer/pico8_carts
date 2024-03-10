a97d22b53a77583e37574f5f42540ad1c24dfde9
aebrer <andrew@brereton.me>
2024-03-09 19:06:19 -0500

    sedi_city_redux init

---
+++ fxhash/sedimentary_city_redux/index.js
@@ -0,0 +1,105 @@
+/*
+This is sedimentary city redux. "sedimentary city" can be found here: https://www.fxhash.xyz/generative/13978
+
+
+Controls:
+
+s -> save a png of the image
+------------------
+
+find my social links, projects, newsletter, roadmap, and more, at aebrer.xyz
+or 
+minted by:
+  eth: 0xD2b1DF574564F4da4775f8bDf79BDF29b42D8BD7
+  tezos: tz1ZBMhTa7gxSpaeXoqyc6bTCrxEHfZYSpPt
+
+license: CC0 -> go nuts; citations not required but definitely appreciated
+
+*/
+
+
+function random_num(a, b) {
+  return a+(b-a)*$fx.rand()
+}
+function random_int(a, b) {
+return Math.floor(random_num(a, b+1))
+}
+
+function randomChoice(arr) {
+return arr[Math.floor(random_num(0,1) * arr.length)];
+}
+
+
+
+const PIX_WIDTH = 256;
+let wth, ww, wh;
+let xfac, yfac;
+
+
+function getColor(x, y) {
+  const index = (y * wth + x) * 4;
+  return [pg.pixels[index], pg.pixels[index + 1], pg.pixels[index + 2], pg.pixels[index + 3]]; // Return a RGBA array
+}
+
+function setColor(x, y, r, g, b) {
+  const index = (y * wth + x) * 4;
+  pg.pixels[index] = r;
+  pg.pixels[index + 1] = g;
+  pg.pixels[index + 2] = b;
+}
+
+
+
+function setup() {
+
+  wth = PIX_WIDTH;
+  if($fx.isPreview){
+    ww=2048
+    mycan = createCanvas(2048, 2048);
+  } else {
+    ww=min(windowWidth, windowHeight)
+    mycan = createCanvas(ww, ww);
+  }
+
+  pg = createGraphics(wth, wth);
+  pg.colorMode(HSL);
+  pg.pixelDensity(1);
+  pg.loadPixels();
+  pixelDensity(1);
+  noSmooth();
+  pg.background(0);
+  pg.strokeWeight(1);
+
+}
+
+function draw() {
+
+  pg.loadPixels();
+
+  if (random_int(1, 1000) > 900) {
+    $fx.reset;
+  }
+
+  for (let x = 0; x < wth; x += random_int(0,8)) {
+  for (let y = 0; y < wth; y += random_int(0,8)) {
+
+      if (random_int(1, 1000) > 50) {
+        $fx.reset;
+      } 
+
+      const c = getColor(x, y);
+      setColor(x, y, (c[0] + random_int(0,1)) % 255, (c[1] + random_int(0,1)) % 255, (c[2] + random_int(0,1)) % 255);
+    }
+  }
+
+  pg.updatePixels();
+  image(pg, 0, 0, ww, ww, 0, 0, wth, wth)
+
+}
+
+// ux
+function keyTyped() {
+if (key === 's') {
+  save("sedimentary_city_redux.png")
+}
+}
